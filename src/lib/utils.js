704a9f1e559845e489d92f4a92b6b8d61a47654a
Muhammad Raza <93082863+muhammadraza30@users.noreply.github.com>
2025-11-15 01:37:58 +0500

    Update src/lib/utils.js

---
+++ src/lib/utils.js
@@ -0,0 +1,20 @@
+import { clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs) {
+  return twMerge(clsx(inputs))
+}
+
+export function generateId() {
+  return Math.random().toString(36).substr(2, 9)
+}
+
+export function formatDate(date) {
+  return new Intl.DateTimeFormat('en-US', {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  }).format(new Date(date))
+}
